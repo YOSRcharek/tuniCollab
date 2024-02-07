b2880ed083d633e532948e9e9da9ab97175a00f8
YOSRcharek <94958031+YOSRcharek@users.noreply.github.com>
2024-02-07 01:13:45 +0100

    init

---
+++ src/Entity/Association.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\AssociationRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: AssociationRepository::class)]
+class Association
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+}
